a88067ecdf08b85d3f7f2cb42e51666b202698f7
GRATHRRAM <101575465+scriptguy1@users.noreply.github.com>
2024-01-25 13:09:30 +0100

    Add files via upload

--- main.asm
+++ main.asm
@@ -6,24 +6,29 @@
 ;nasm -f elf64 -o main.o main.asm
 ;ld -o main main.o
 
+STDOUT equ 1
+WRITE  equ 1
+
 global _start
 section .rodata
   wellcome_msg db "Guess The Number -- By GRATHRRAM", 10
-  enter_msg  db "Enter max number that can be generated: ", 10
-  numlow_msg db "Number is too low!", 10
-  numbig_msg db "Number is too Big!", 10
-  numbis_msg db "You did it number is ", 10
-
   wellcome_len equ $- wellcome_msg  
+  enter_msg  db "Enter max number that can be generated: ", 10
   enter_len  equ $- enter_msg
-  numlow_len equ $- numlow_msg 
+  numlow_msg db "Number is too low!", 10
+  numlow_len equ $- numlow_msg
+  numbig_msg db "Number is too Big!", 10
   numbig_len equ $- numbig_msg
+  numbis_msg db "You did it number is ", 10
   numbis_len equ $- numbis_msg
+
+section .bss
+  buff resb 16
 
 section .text
 _start:
-  mov rax, 1        ; write syscall
-  mov rdi, 1        ; STDOUT_FILENO,
+  mov rax, WRITE
+  mov rdi, STDOUT
   mov rsi, wellcome_msg
   mov rdx, wellcome_len
   syscall
